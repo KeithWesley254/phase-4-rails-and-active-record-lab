2b3229c3aa9b444cae7ec4e9c4d167a7218d065e
Keith <keithwesley187@gmail.com>
2022-09-26 14:51:28 +0300

    Done

---
+++ app/models/student.rb
@@ -0,0 +1,10 @@
+class Student < ApplicationRecord
+
+    def to_s
+        first_name = self.first_name
+        last_name = self.last_name
+        
+        first_name + " " + last_name
+    end
+
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+rambo= Student.create(first_name: "Sylvester", last_name: "Stalone", grade: 80)
